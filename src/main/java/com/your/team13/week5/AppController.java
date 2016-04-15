5ef6d253ef558e7e3f6d3400f03724a60f336b96
sangjun2 <sj27294626@gmail.com>
2016-04-15 15:37:34 +0900

    commit

---
+++ src/main/java/com/your/team13/week5/AppController.java
@@ -0,0 +1,46 @@
+package com.your.team13.week5;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppController {
+	private static final Logger logger = Logger.getLogger("PrintBill");
+	private StringBuffer buf;
+	
+	Plan plan;
+	Customer customer;
+	Calculate calculate;
+
+	public AppController() {
+
+	}
+
+	public void run(String inputPlan, int callTime, int lineNumber) {
+		customer = new Customer();
+		
+		customer.setPlan(getPlan(inputPlan));
+		customer.setCallTime(callTime);
+		customer.setlineNumber(lineNumber);
+		
+		calculate = new Calculate(customer);
+		
+		this.printBill();
+	}
+
+	public Plan getPlan(String inputPlan) {
+		if ("gold".equals(inputPlan.toLowerCase())) {
+			this.plan = new Gold();
+		} else if ("silver".equals(inputPlan.toLowerCase())) {
+			this.plan = new Silver();
+		}
+
+		return this.plan;
+	}
+
+	public void printBill() {
+		buf = new StringBuffer("=================================");
+		buf.append("<Phone Bill>");
+		buf.append("Plan : ");
+		System.out.printf("금액 : %.2f\n", calculate.totalRate());
+	}
+}
--- src/main/java/com/your/team13/week5/Customer.java
+++ src/main/java/com/your/team13/week5/Customer.java
@@ -6,25 +6,39 @@ public class Customer {
 
 	protected Plan plan;
 
+	public Customer() {
+		this.plan = null;
+		this.callTime = 0;
+		this.lineNumber = 0; 
+	}
+	
 	public Customer(Plan plan, int callTime, int lineNumber) {
 		this.plan = plan;
 		this.callTime = callTime;
 		this.lineNumber = lineNumber;
+	}
+	
+	public Plan getPlan() {
+		return plan;
+	}
+	
+	public void setPlan(Plan plan) {
+		this.plan = plan;
 	}
 
 	public int getlineNumber() {
 		return lineNumber;
 	}
 
-//	public void setlineNumber(int lineNumber) {
-//		this.lineNumber = lineNumber;
-//	}
+	public void setlineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
 
 	public int getCallTime() {
 		return callTime;
 	}
 
-//	public void setCallTime(int callTime) {
-//		this.callTime = callTime;
-//	}
+	public void setCallTime(int callTime) {
+		this.callTime = callTime;
+	}
 }
